de276f6f34926ca6f0f17857076d90ed4414213e
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-07-25 19:07:20 +0200

    Adjsuted accordian to close and open

--- script.js
+++ script.js
@@ -42,17 +42,27 @@ fetchColorThemes();
 // ACCORDIAN LOGIG
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll(".accordion-item");
+  let activeItem = null;
 
   accordionItems.forEach(function (item) {
     const header = item.querySelector(".accordion-header");
 
     header.addEventListener("click", function () {
-      accordionItems.forEach(function (item) {
-        item.querySelector(".accordion-content").classList.remove("active");
-      });
-
       const content = item.querySelector(".accordion-content");
-      content.classList.toggle("active");
+      const isActive = content.classList.contains("active");
+
+      if (isActive) {
+        content.classList.remove("active");
+        activeItem = null;
+      } else {
+        if (activeItem) {
+          activeItem
+            .querySelector(".accordion-content")
+            .classList.remove("active");
+        }
+        content.classList.add("active");
+        activeItem = item;
+      }
     });
   });
 });
