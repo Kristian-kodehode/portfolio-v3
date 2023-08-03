d5929377895af90b7422e9dd95bb45ef4d962ae6
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-03 08:28:42 +0200

    added ui card data js. imported to script file

---
+++ projectsData.js
@@ -0,0 +1,45 @@
+const projectsData = {
+  projects: [
+    {
+      id: 1,
+      img: "images/tictactoe.png",
+      heading: "Game : Tic Tac Toe",
+      description:
+        "The classic game, tic tac toe with a modern and bright look. This started out as a tutorial on React and i expanded it from there.",
+      showHtml: true,
+      showCss: true,
+      showJs: true,
+      showReact: true,
+      showFigma: true,
+      githubLink: "https://kristian-kodehode.github.io/react-tictactoe/",
+    },
+    {
+      id: 2,
+      img: "images/snake.png",
+      heading: "Game : Snake",
+      description:
+        "The classic game, Snake with a modern look. This started out as a tutorial and i expanded it quite alot from there.",
+      showHtml: true,
+      showCss: true,
+      showJs: true,
+      showReact: true,
+      showFigma: true,
+      githubLink: "https://kristian-kodehode.github.io/game-snake/",
+    },
+    {
+      id: 3,
+      img: "images/stopwatch.png",
+      heading: "App : Stopwatch",
+      description:
+        "Yet another classic project, the stopwatch. For the style concious person that loves to even have the stopwatch look good.",
+      showHtml: true,
+      showCss: true,
+      showJs: true,
+      showReact: true,
+      showFigma: true,
+      githubLink: "https://kristian-kodehode.github.io/02_Stopwatch/",
+    },
+  ],
+};
+
+export { projectsData };
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentThemeIndex = 0;
 let colorThemes = [];
 
+// THEME CHANGE LOGIC
 const fetchColorThemes = async () => {
   try {
     const response = await fetch("color-themes.json");
@@ -38,12 +39,84 @@ const changeColors = () => {
     currentTheme["col-secondary80"]
   );
 };
-
 document.getElementById("themeSelect").addEventListener("change", changeColors);
 
 fetchColorThemes();
 
+/*
+
+
+
+
+
+// UI CARDS DISTRIBUTION
+*/
+import { projects } from "./projectsData.js";
+
+const projectContainer = document.getElementById("ui-cards-container");
+
+const projectCards = projects.map((project) => {
+  const card = document.createElement("div");
+  card.classList.add("project-card");
+  card.setAttribute("key", project.id);
+
+  // Create other elements for the UI cards (image, heading, description)
+
+  const iconsContainer = document.createElement("div");
+
+  const addIcon = (iconClass) => {
+    const icon = document.createElement("i");
+    icon.classList.add(iconClass);
+    iconsContainer.appendChild(icon);
+  };
+
+  switch (true) {
+    case project.showIcon1:
+      addIcon("fa-icon1"); // Replace 'fa-icon1' with the appropriate class for icon 1
+      break;
+    case project.showIcon2:
+      addIcon("fa-icon2"); // Replace 'fa-icon2' with the appropriate class for icon 2
+      break;
+    case project.showIcon3:
+      addIcon("fa-icon3"); // Replace 'fa-icon3' with the appropriate class for icon 3
+      break;
+    case project.showIcon4:
+      addIcon("fa-icon4"); // Replace 'fa-icon4' with the appropriate class for icon 4
+      break;
+    case project.showIcon5:
+      addIcon("fa-icon5"); // Replace 'fa-icon5' with the appropriate class for icon 5
+      break;
+    default:
+      break;
+  }
+
+  card.appendChild(iconsContainer);
+
+  // Create the button with the link from the 'buttonLink' property
+  const buttonLink = document.createElement("a");
+  buttonLink.href = project.buttonLink;
+  buttonLink.target = "_blank";
+
+  const button = document.createElement("button");
+  button.classList.add("ui-card-button");
+  button.textContent = "Experience Project";
+
+  buttonLink.appendChild(button);
+  card.appendChild(buttonLink);
+
+  return card;
+});
+
+projectContainer.append(...projectCards);
+
+/*
+
+
+
+
+
 // ACCORDIAN LOGIG
+*/
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll(".accordion-item");
   let activeItem = null;
