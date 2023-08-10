7116f11ab6c09b17bac8f7f62508d0de647b9f4a
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-10 14:39:14 +0200

    trying new theme switcher

--- script.js
+++ script.js
@@ -1,5 +1,63 @@
-// import { projectsData } from "./projectsData.js";
+const themeSelect = document.getElementById("themeSelect");
+const prevThemeButton = document.getElementById("prevTheme");
+const nextThemeButton = document.getElementById("nextTheme");
+let colorThemes = []; // Store your color themes here
+let currentThemeIndex = 0;
 
+// Fetch color themes from the JSON data
+const fetchColorThemes = async () => {
+  try {
+    // Replace the URL with the correct path to your JSON file
+    const response = await fetch("color-themes.json");
+    colorThemes = await response.json();
+    changeColors(); // Apply default theme
+  } catch (error) {
+    console.log("Error fetching color themes:", error);
+  }
+};
+
+// Handle previous theme button click
+prevThemeButton.addEventListener("click", () => {
+  if (currentThemeIndex > 0) {
+    currentThemeIndex -= 1;
+    changeColors();
+  }
+});
+
+// Handle next theme button click
+nextThemeButton.addEventListener("click", () => {
+  if (currentThemeIndex < colorThemes.length - 1) {
+    currentThemeIndex += 1;
+    changeColors();
+  }
+});
+
+// Function to apply the selected theme
+const changeColors = () => {
+  const currentTheme = colorThemes[currentThemeIndex];
+  // Set CSS variables using currentTheme values
+  document.documentElement.style.setProperty(
+    "--col-primary",
+    currentTheme["col-primary"]
+  );
+  document.documentElement.style.setProperty(
+    "--col-primary80",
+    currentTheme["col-primary80"]
+  );
+  document.documentElement.style.setProperty(
+    "--col-secondary",
+    currentTheme["col-secondary"]
+  );
+  document.documentElement.style.setProperty(
+    "--col-secondary80",
+    currentTheme["col-secondary80"]
+  );
+};
+
+fetchColorThemes();
+
+/*
+/////////////////////////
 let currentThemeIndex = 0;
 let colorThemes = [];
 
@@ -44,6 +102,8 @@ const changeColors = () => {
 document.getElementById("themeSelect").addEventListener("change", changeColors);
 
 fetchColorThemes();
+////////////////////
+*/
 
 /*
 
