e0933dd64395ac4461e2ca9640a77a6edd7f97d9
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-17 18:11:16 +0200

    simplified acc code

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-/*
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+  let activeItem = null;
 
+  accordionItems.forEach((item) => {
+    const header = item.querySelector(".accordion-header");
+
+    header.addEventListener("click", () => {
+      const content = item.querySelector(".accordion-content");
+
+      if (activeItem && activeItem !== item) {
+        activeItem
+          .querySelector(".accordion-content")
+          .classList.remove("active");
+      }
+
+      content.classList.toggle("active");
+      activeItem = item;
+    });
+  });
+});
+
+/* OLD
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll(".accordion-item");
 
@@ -7,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   accordionItems.forEach(function (item) {
     const header = item.querySelector(".accordion-header");
-    const icons = item.querySelector(".accordian-icons");
 
     header.addEventListener("click", function () {
       const content = item.querySelector(".accordion-content");
@@ -15,52 +35,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (isActive) {
         content.classList.remove("active");
-        icons.style.transform = "rotate(0deg)";
         activeItem = null;
+        console.log("IsActive 1");
       } else {
         if (activeItem) {
           activeItem
             .querySelector(".accordion-content")
             .classList.remove("active");
-          icons.style.transform = "rotate(0deg)";
         }
         content.classList.add("active");
-        icons.style.transform = "rotate(180deg)";
         activeItem = item;
+        console.log("IsActive 3");
       }
     });
   });
 });
 */
-document.addEventListener("DOMContentLoaded", function () {
-  const accordionItems = document.querySelectorAll(".accordion-item");
-
-  let activeItem = null;
-
-  accordionItems.forEach(function (item) {
-    const header = item.querySelector(".accordion-header");
-    const icons = item.querySelector(".accordian-icons");
-
-    header.addEventListener("click", function () {
-      const content = item.querySelector(".accordion-content");
-      const isActive = content.classList.contains("active");
-
-      if (isActive) {
-        content.classList.remove("active");
-        icons.style.transform = "rotate(0deg)";
-        activeItem = null;
-      } else {
-        if (activeItem) {
-          activeItem
-            .querySelector(".accordion-content")
-            .classList.remove("active");
-          activeItem.querySelector(".accordian-icons").style.transform =
-            "rotate(0deg)";
-        }
-        content.classList.add("active");
-        icons.style.transform = "rotate(180deg)";
-        activeItem = item;
-      }
-    });
-  });
-});
