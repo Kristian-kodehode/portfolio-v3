3fad7317dc106c40b984d70c55aa512b938da514
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-08-17 17:43:45 +0200

    messing with the acc icons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// ACCORDIAN LOGIG
+/*
+
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll(".accordion-item");
 
@@ -6,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   accordionItems.forEach(function (item) {
     const header = item.querySelector(".accordion-header");
+    const icons = item.querySelector(".accordian-icons");
 
     header.addEventListener("click", function () {
       const content = item.querySelector(".accordion-content");
@@ -13,14 +15,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (isActive) {
         content.classList.remove("active");
+        icons.style.transform = "rotate(0deg)";
         activeItem = null;
       } else {
         if (activeItem) {
           activeItem
             .querySelector(".accordion-content")
             .classList.remove("active");
+          icons.style.transform = "rotate(0deg)";
         }
         content.classList.add("active");
+        icons.style.transform = "rotate(180deg)";
+        activeItem = item;
+      }
+    });
+  });
+});
+*/
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+
+  let activeItem = null;
+
+  accordionItems.forEach(function (item) {
+    const header = item.querySelector(".accordion-header");
+    const icons = item.querySelector(".accordian-icons");
+
+    header.addEventListener("click", function () {
+      const content = item.querySelector(".accordion-content");
+      const isActive = content.classList.contains("active");
+
+      if (isActive) {
+        content.classList.remove("active");
+        icons.style.transform = "rotate(0deg)";
+        activeItem = null;
+      } else {
+        if (activeItem) {
+          activeItem
+            .querySelector(".accordion-content")
+            .classList.remove("active");
+          activeItem.querySelector(".accordian-icons").style.transform =
+            "rotate(0deg)";
+        }
+        content.classList.add("active");
+        icons.style.transform = "rotate(180deg)";
         activeItem = item;
       }
     });
