04202fb1fe1728242ded5e42afd4fe3f1d8820b5
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-07-03 14:37:53 +0200

    udpated about text. updated cv link. updated intro text

--- projectsData.js
+++ projectsData.js
@@ -5,7 +5,7 @@ const projectsData = {
       img: "images/solcellekart2.webp",
       heading: "Saas : Solcellekart (2024)",
       description:
-        "Hjelper deg å finne beste pris på solcelleløsninger. Et av prosjektene jeg jobbet på hos Solintegra. Rolle: Frontend | UX",
+        "Hjelper deg å finne riktig solcelleløsning til ditt tak. Jeg hadde hovedansvar for dette prosjektet hos Solintegra. Rolle: Frontend | UX",
       showHtml: false,
       showCss: false,
       showJs: true,
